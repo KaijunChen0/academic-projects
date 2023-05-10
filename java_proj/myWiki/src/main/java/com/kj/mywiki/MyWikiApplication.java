9616deff162a2b67f268c9271821277b38084849
KaijunChen0 <113864900+KaijunChen0@users.noreply.github.com>
2023-05-10 00:12:36 -0700

    modify startup intro

--- java_proj/myWiki/src/main/java/com/kj/mywiki/MyWikiApplication.java
+++ java_proj/myWiki/src/main/java/com/kj/mywiki/MyWikiApplication.java
@@ -1,13 +1,22 @@
 package com.kj.mywiki;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import org.slf4j.Logger;
 
 @SpringBootApplication
 public class MyWikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MyWikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(MyWikiApplication.class, args);
+//        SpringApplication.run(MyWikiApplication.class, args);
+        SpringApplication app = new SpringApplication(MyWikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Startup successfully!");
+        LOG.info("Location:\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
